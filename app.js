16f70e68a3c77d6bc90202b944e41d2776460dc3
urbannomad94 <95559192+urbannomad94@users.noreply.github.com>
2022-01-22 13:28:07 -0500

    updated changeGridSize function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Declaring variables
 const gridContainer = document.querySelector('.grid-container');
 const buttonContainer = document.querySelector('.buttons');
 const topButtons = document.querySelector('.top-buttons');
@@ -5,33 +6,42 @@ const btnBlack = document.createElement('button');
 const btnGray = document.createElement('button');
 const btnRainbow = document.createElement('button');
 //const btnColor = document.createElement('button');
-const btnGridSize = document.createElement('button');
 const btnEraser = document.createElement('button');
-const btnReset = document.createElement('button');
-const btnGridLines = document.createElement('button');
+const btnClear = document.createElement('button');
+const btnGridSize = document.createElement('button');
+const btnGridlines = document.createElement('button');
 
+//Makes a grid of divs
 function makeTable (cols, rows){
     for (let i = 0; i < (cols * rows); i++){
         const div = document.createElement('div');
         div.style.border = '1px solid gray';
         gridContainer.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
-        gridTemplateRows = `repeat(${rows}, 1fr)`
+        gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`
         gridContainer.appendChild(div).classList.add('box');
     }
 }
 makeTable(16,16);
 
+//Button that promps user for desired grid dimension
 function changeGridSize() {
     btnGridSize.textContent = "Change Grid Size"
     btnGridSize.addEventListener('click', () => {
         let x = prompt("How many rows and columns would you like the grid to have?");
-        makeTable(x,x);
+        if (x === null || x < 1 || x > 100) {
+            reset();
+            makeTable(16,16);
+        }
+        else {
+            makeTable(x,x);
+        }
     })
     topButtons.appendChild(btnGridSize).classList.add('btn');
 }
 
 changeGridSize();
 
+//Button that lets the user draw in grayscale
 function grayColor() {
     const boxes = gridContainer.querySelectorAll('.box');
     btnGray.textContent = 'Grey';
@@ -46,6 +56,7 @@ function grayColor() {
 
 grayColor();
 
+//Button that lets the user draw in "rainbow"
 function rainbowColor() {
     const boxes = gridContainer.querySelectorAll('.box');
     btnRainbow.textContent = 'Rainbow';
@@ -60,6 +71,7 @@ function rainbowColor() {
 
 rainbowColor();
 
+//Button that lets the user draw in black
 function blackColor() {
     const boxes = gridContainer.querySelectorAll('.box');
     btnBlack.textContent = 'Black';
@@ -73,6 +85,7 @@ function blackColor() {
 
 blackColor();
 
+//Button that allows the user to erase what's on the grid (really just coloring in white)
 function eraser() {
     const boxes = gridContainer.querySelectorAll('.box');
     btnEraser.textContent = 'Eraser';
@@ -86,14 +99,35 @@ function eraser() {
 
 eraser();
 
-function reset() {
+//Button that clears the grid
+function clear() {
     const boxes = gridContainer.querySelectorAll('.box');
-    btnReset.textContent = 'Reset';
-    btnReset.addEventListener('click', () =>{ 
+    btnClear.textContent = 'Clear';
+    btnClear.addEventListener('click', () =>{ 
         boxes.forEach(box => box.style.background = 'white');
         }
     )
-    topButtons.appendChild(btnReset).classList.add('btn');
+    topButtons.appendChild(btnClear).classList.add('btn')
 }
 
-reset();
+clear();
+
+function reset() {
+    const boxes = gridContainer.querySelectorAll('.box');
+    boxes.forEach(box => box.remove())
+}
+
+    //Trying to make the border white so the grid disappers ///Not functioning yet
+
+    /*
+function toggleGridLines() {
+    const boxes = gridContainer.querySelectorAll('.box');
+    btnGridlines.textContent = 'Toggle Gridlines';
+    btnGridlines.addEventListener('click', () => {
+        boxes.style.border = 'white';
+    })
+    topButtons.appendChild(btnGridlines).classList.add('btn');
+}
+
+toggleGridLines();
+    */
